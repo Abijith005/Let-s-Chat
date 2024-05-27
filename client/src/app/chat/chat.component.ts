1a1981e3f09fb573c997db2515ddb560f7ddbd67
abijith <abijithsurendran005@gmail.com>
2024-05-27 10:19:11 +0530

    home component completed

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
-  standalone: true,
-  imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
 
